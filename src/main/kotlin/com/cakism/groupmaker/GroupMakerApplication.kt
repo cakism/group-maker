9efe7292b644f099b5f235d42cc2173aaeb459af
jenola <weiS9ceikaQWE>
2018-10-31 10:09:42 +0100

    first commit

---
+++ src/main/kotlin/com/cakism/groupmaker/GroupMakerApplication.kt
@@ -0,0 +1,11 @@
+package com.cakism.groupmaker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroupMakerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GroupMakerApplication>(*args)
+}
---
+++ src/test/kotlin/com/cakism/groupmaker/GroupMakerApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.cakism.groupmaker
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GroupMakerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
